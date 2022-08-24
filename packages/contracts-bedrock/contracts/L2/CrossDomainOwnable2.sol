06afad3ddaecae73dc6f6fca41eb195451cd3669
Mark Tyneway <mark.tyneway@gmail.com>
2022-08-24 14:06:44 -0700

    contracts-bedrock: CrossDomainOwnable
    Adds 2 new contracts to the `L2` directory. Both contracts
    extend OZ `Ownable` such that the `onlyOwner` modifer logic is
    overwritten.
    
    `CrossDomainOwnable` is designed for ownership of contracts on L2
    when the communication goes directly through the `OptimismPortal`.
    This is not safe for when the communication goes through the
    `CrossDomainMessenger` system, as `msg.sender` would be the
    address of the `L2CrossDomainMessenger` and if that is the owner
    of the L2 contract then anybody going through the messenger system
    would have owner permissions on the contract.
    
    `CrossDomainOwnable2` is designed for ownership of contracts on L2
    when the communication goes through the messenger system. It checks
    for the L1 message sender value that is set in the
    `L2CrossDomainMessenger` contract.
    
    Both of these contracts include test coverage and are relatively simple.

---
+++ packages/contracts-bedrock/contracts/L2/CrossDomainOwnable2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { Predeploys } from "../libraries/Predeploys.sol";
+import { L2CrossDomainMessenger } from "./L2CrossDomainMessenger.sol";
+import { AddressAliasHelper } from "../vendor/AddressAliasHelper.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title CrossDomainOwnable2
+ * @notice This contract extends the OpenZeppelin `Ownable` contract
+ *         for L2 contracts to be owned by contracts on L1. Note that
+ *         this contract is meant to be used with systems that use the
+ *         CrossDomainMessenger system. It will not work if the OptimismPortal
+ *         is used directly.
+ */
+abstract contract CrossDomainOwnable2 is Ownable {
+    L2CrossDomainMessenger internal messenger = L2CrossDomainMessenger(
+        Predeploys.L2_CROSS_DOMAIN_MESSENGER
+    );
+
+    /**
+     * @notice Overrides the implementation of the `onlyOwner` modifier
+     *         to check that the unaliased `xDomainMessageSender`
+     *         is the owner of the contract. This value is set
+     *         to the caller of the L1CrossDomainMessenger.
+     */
+    function _checkOwner() internal override view {
+        address sender = AddressAliasHelper.undoL1ToL2Alias(
+            messenger.xDomainMessageSender()
+        );
+        require(
+            owner() == sender,
+            "CrossDomainOwnable2: caller is not the owner"
+        );
+    }
+}
